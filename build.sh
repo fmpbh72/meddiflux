343ab1ba5cc5970d5f1a0e88c52ec4a9d94bb63b
Fernando Passos <fmpassos@gmail.com>
2024-11-12 01:54:17 -0300

    ajuste em hom

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Verifica o argumento passado para definir a URL da API
-if [ "$1" == "prod" ]; then
-  export REACT_APP_API_URL=http://prodfmp.us-east-1.elasticbeanstalk.com
-else
-  export REACT_APP_API_URL=http://homfmp.us-east-1.elasticbeanstalk.com
+URL=http://prodfmp.us-east-1.elasticbeanstalk.com
+if [ "$1" == "hom" ]; then
+  export URL=http://homfmp.us-east-1.elasticbeanstalk.com
 fi
 
 # Instala as dependências e constrói o projeto
 npm install --loglevel=error
-REACT_APP_API_URL=$REACT_APP_API_URL SKIP_PREFLIGHT_CHECK=true npm run build --prefix client
+REACT_APP_API_URL=$URL SKIP_PREFLIGHT_CHECK=true npm run build --prefix client
