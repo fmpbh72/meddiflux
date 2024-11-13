19f06db6b8c220340cdf4bfffcac3e4f270ae5a4
Fernando Passos <fmpassos@gmail.com>
2024-11-12 23:04:51 -0300

    novo2

--- build_old.sh
+++ build_old.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 # Verifica o argumento passado para definir a URL da API
-
-URL=http://prodfmp.us-east-1.elasticbeanstalk.com
-
-if [ "EVENTO" == "homfmp" ]; then
-  export URL=http://homfmp.us-east-1.elasticbeanstalk.com
-fi
-
+#URL=http://prodfmp.us-east-1.elasticbeanstalk.com
+#if [ "EVENTO" == "homfmp" ]; then
+#  export URL=http://homfmp.us-east-1.elasticbeanstalk.com
+#fi
 # Instala as dependências e constrói o projeto
 npm install --loglevel=error
 - echo "$URL"
-REACT_APP_API_URL=$URL SKIP_PREFLIGHT_CHECK=true npm run build --prefix client
+REACT_APP_API_URL=$API_URL SKIP_PREFLIGHT_CHECK=true npm run build --prefix client
