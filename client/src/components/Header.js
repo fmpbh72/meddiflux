10ab221a83c9642a83482d8a0bcf1dfe6af725cd
Fernando Passos <fmpassos@gmail.com>
2024-11-13 20:53:12 -0300

    other

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,7 +11,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {location.pathname === "/" && (
         <Button
           color={showAdd ? "red" : "green"}
-          text={showAdd ? "Closed..." : "Adicionar/Open"}
+          text={showAdd ? "Closed.." : "Adicionar/Open"}
           onClick={onAdd}
         />
       )}
