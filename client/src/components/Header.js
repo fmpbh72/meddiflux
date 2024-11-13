926e189c56cb4bd4a89b9126e5ea4640552e00bb
Fernando Passos <fmpassos@gmail.com>
2024-11-13 00:17:56 -0300

    Novo teste

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,8 +10,8 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "red" : "green"}
-          text={showAdd ? "Fechar" : "Adicionar"}
+          color={showAdd ? "orange" : "green"}
+          text={showAdd ? "Fechado/Closed" : "Adicionar/Open"}
           onClick={onAdd}
         />
       )}
