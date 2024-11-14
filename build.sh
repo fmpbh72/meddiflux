d6aa7432b2e7837769d6857318e74ec2747979e9
Fernando Passos <fmpassos@gmail.com>
2024-11-13 22:53:08 -0300

    eco

--- build.sh
+++ build.sh
@@ -13,6 +13,10 @@ elif [ "$1" == "prod" ]; then
 export REACT_APP_API_URL=http://homfmp.us-east-1.elasticbeanstalk.com
 fi
 
+echo "Usando a URL da API: $REACT_APP_API_URL"
+
+echo "Ambiente: $ENVIROMENT"
+ 
 # Instala as dependências e constrói o projeto
 npm install --loglevel=error
 NODE_OPTIONS=--openssl-legacy-provider REACT_APP_API_URL=$REACT_APP_API_URL SKIP_PREFLIGHT_CHECK=true npm run build --prefix client
