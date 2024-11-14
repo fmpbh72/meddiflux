70375e7cf8b8ffbb534a03b5e72b8513d4633e06
Fernando Passos <fmpassos@gmail.com>
2024-11-13 22:09:55 -0300

    tirando

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Verifica o argumento passado para definir a URL da API
-#URL=http://prodfmp.us-east-1.elasticbeanstalk.com
+# URL=http://prodfmp.us-east-1.elasticbeanstalk.com
 #if [ "EVENTO" == "homfmp" ]; then
 #  export URL=http://homfmp.us-east-1.elasticbeanstalk.com
 #fi
