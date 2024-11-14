3e9f43fee4d894742f87bf69a42bd95bf71983d5
Fernando Passos <fmpassos@gmail.com>
2024-11-13 22:39:53 -0300

    nbuild

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 # Verifica o argumento passado para definir a URL da API
-if [ "$1" == "prod" ]; then
-    export REACT_APP_API_URL=http://prodfmp.us-east-1.elasticbeanstalk.com
-else
-    export REACT_APP_API_URL=http://homfmp.us-east-1.elasticbeanstalk.com
+#if [ "$1" == "prod" ]; then
+#    export REACT_APP_API_URL=http://prodfmp.us-east-1.elasticbeanstalk.com
+#else
+#    export REACT_APP_API_URL=http://homfmp.us-east-1.elasticbeanstalk.com
+#fi
+
+if [ "$1" == "prod" ]; then 
+export REACT_APP_API_URL=http://prodfmp.us-east-1.elasticbeanstalk.com
+elif [ "$1" == "hom" ]; then
+export REACT_APP_API_URL=http://homfmp.us-east-1.elasticbeanstalk.com
 fi
+
 # Instala as dependências e constrói o projeto
 npm install --loglevel=error
 NODE_OPTIONS=--openssl-legacy-provider REACT_APP_API_URL=$REACT_APP_API_URL SKIP_PREFLIGHT_CHECK=true npm run build --prefix client
