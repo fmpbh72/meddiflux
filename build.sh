654d7e3b5d2421748d6acb23ab57baab145690f2
Alisson Rios <alisrios@gmail.com>
2024-11-13 22:52:15 -0300

    ajuste do Dockerfile again 4

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ elif [ "$1" == "hom" ]; then
 export REACT_APP_API_URL=https://hom.projeto-aws.com.br
 fi
 
+echo Construindo para o ambiente $ENVIRONMENT
+
 # Instala as dependências e constrói o projeto
 npm install --loglevel=error
 NODE_OPTIONS=--openssl-legacy-provider REACT_APP_API_URL=$REACT_APP_API_URL SKIP_PREFLIGHT_CHECK=true npm run build --prefix client
