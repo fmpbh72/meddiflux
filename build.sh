ca3c2984631baf4003d636feae8e5ef393f27ea3
Fernando Passos <fmpassos@gmail.com>
2024-11-16 01:15:44 -0300

    TENTANDO

--- build.sh
+++ build.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-#echo "Ambiente: $ENVIRONMENT"
-
-#if [ $ENVIRONMENT == "prod" ]; then 
-#export REACT_APP_API_URL=http://prodfmp.us-east-1.elasticbeanstalk.com
-#else
-#export REACT_APP_API_URL=http://homfmp.us-east-1.elasticbeanstalk.com
-#fi
-
-# Define a URL com base no ambiente
-ENVIROMENT=$ENVIRONMENT
-
-if [ "$ENVIRONMENT" == "hom" ]; then
-REACT_APP_API_URL="http://homfmp.us-east-1.elasticbeanstalk.com"
-else [ "$ENVIRONMENT" == "prod" ]; then
-REACT_APP_API_URL="http://prodfmp.us-east-1.elasticbeanstalk.com"
+if [ $ENVIRONMENT == "prod" ]; then 
+REACT_APP_API_URL=http://prodfmp.us-east-1.elasticbeanstalk.com
+else
+REACT_APP_API_URL=http://homfmp.us-east-1.elasticbeanstalk.com
 fi
 
 # Exibe a URL
+
 echo "A URL para o ambiente $ENVIRONMENT é: $REACT_APP_API_URL"
 
 # Instala as dependências e constrói o projeto
+
 npm install --loglevel=error
 
 NODE_OPTIONS=--openssl-legacy-provider REACT_APP_API_URL=$REACT_APP_API_URL SKIP_PREFLIGHT_CHECK=true npm run build --prefix client
