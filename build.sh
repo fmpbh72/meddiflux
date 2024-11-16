8287f0d073dccd789ecde05388e8ca4b83803fb8
Fernando Passos <fmpassos@gmail.com>
2024-11-15 23:20:28 -0300

    Ajuste 2320

--- build.sh
+++ build.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
 
-# Verifica o argumento passado para definir a URL da API
-#if [ "$1" == "prod" ]; then
-#    export REACT_APP_API_URL=http://prodfmp.us-east-1.elasticbeanstalk.com
-#else
-#    export REACT_APP_API_URL=http://homfmp.us-east-1.elasticbeanstalk.com
-#fi.
+echo "Ambiente: $ENVIRONMENT"
 
-if [ "$ENVIRONMENT" == "prod" ]; then 
+if [ $ENVIRONMENT == "prod" ]; then 
 export REACT_APP_API_URL=http://prodfmp.us-east-1.elasticbeanstalk.com
-elif [ "$1" == "hom" ]; then
+else
 export REACT_APP_API_URL=http://homfmp.us-east-1.elasticbeanstalk.com
 fi
 
-#if [ "$1" == "prod" ]; then 
-#NODE_OPTIONS=--openssl-legacy-provider REACT_APP_API_URL=http://prodfmp.us-east-1.elasticbeanstalk.com SKIP_PREFLIGHT_CHECK=true npm run build --prefix client#export 
-#elif [ "$1" == "hom" ]; then
-#NODE_OPTIONS=--openssl-legacy-provider REACT_APP_API_URL=http://homfmp.us-east-1.elasticbeanstalk.com SKIP_PREFLIGHT_CHECK=true npm run build --prefix client#export 
-#fi
-
 echo "Usando a URL da API: $REACT_APP_API_URL"
-
-echo "Ambiente: $ENVIRONMENT"
 
 # Instala as dependências e constrói o projeto
 npm install --loglevel=error
-
-REACT_APP_API_URL=http://homfmp.us-east-1.elasticbeanstalk.com
 
 NODE_OPTIONS=--openssl-legacy-provider REACT_APP_API_URL=$REACT_APP_API_URL SKIP_PREFLIGHT_CHECK=true npm run build --prefix client
